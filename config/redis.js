cbd308af614383c06958958087f8e8dace7e5a51
Dao Le Quang <dao.le@pycogroup.com>
2017-10-07 14:59:29 +0700

    update redis

--- config/redis.js
+++ config/redis.js
@@ -10,11 +10,13 @@ console.log('REDIS URL =', redis_url);
 
 let client;
 if (redis_url !== undefined) {
- client = Redis.createClient(redis_url);
+  console.log('START CONNECT TO REDIS =', redis_url);
+  client = Redis.createClient(redis_url);
 } else {
   console.log('CONNECT REDIS =', port, ' HOST =', hostname);
   client = Redis.createClient(port, hostname);
 }
+console.log('CLIENT REDIS =', client);
 
 client.on('error', (err) => {
   console.error(`Couldn't connect to redis ${err}`);
